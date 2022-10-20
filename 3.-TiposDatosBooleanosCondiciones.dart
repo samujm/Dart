a15397891144a9f599756ffe6bb1fd4ef9860139
Samantha González <samujm76@gmail.com>
2022-10-19 19:10:07 -0500

    Tipos de datos Booleanos y condiciones

---
+++ 3.-TiposDatosBooleanosCondiciones.dart
@@ -0,0 +1,12 @@
+void main() {
+  //Verdadero o falso
+
+  //Colocar ? despues del bool para que bool acepte valor null
+  bool? isActive = null;
+
+  if (isActive == null) {
+    print('isActive es null');
+  } else {
+    print('No es null');
+  }
+}
